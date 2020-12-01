9b78b45d0e51798d105d1aed1998fda35d2fba4e
Zuzia68 <61715307+Zuzia68@users.noreply.github.com>
2020-12-01 11:09:44 +0100

    Update Program.cs
    Dodawanie tła

--- Wypożyczalnia samochodów/Program.cs
+++ Wypożyczalnia samochodów/Program.cs
@@ -1,23 +1,22 @@
   
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace Wypozyczalnia_Samochodow
-{
-    static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MenuGlowne());
-        }
-    }
-}
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio 2013
+VisualStudioVersion = 12.0.21005.1
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "Wypozyczalnia Samochodow", "Wypozyczalnia Samochodow\Wypozyczalnia Samochodow.csproj", "{95363ACF-3368-4DEB-A4AE-B4D92F2C71F2}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|Any CPU = Debug|Any CPU
+		Release|Any CPU = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{95363ACF-3368-4DEB-A4AE-B4D92F2C71F2}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{95363ACF-3368-4DEB-A4AE-B4D92F2C71F2}.Debug|Any CPU.Build.0 = Debug|Any CPU
+		{95363ACF-3368-4DEB-A4AE-B4D92F2C71F2}.Release|Any CPU.ActiveCfg = Release|Any CPU
+		{95363ACF-3368-4DEB-A4AE-B4D92F2C71F2}.Release|Any CPU.Build.0 = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+EndGlobal
