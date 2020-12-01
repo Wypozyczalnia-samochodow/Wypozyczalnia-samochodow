d32da7b45ec695e12c7ce0e612bbbf4c06ef6586
ojoj00 <71120940+ojoj00@users.noreply.github.com>
2020-12-01 10:48:23 +0100

    Dodanie operacji usuwania rekordu z bazy danych

---
+++ Wypożyczalnia samochodów/MenuGlowne.Designer.cs
@@ -0,0 +1,106 @@
+namespace Wypozyczalnia_Samochodow
+{
+    partial class MenuGlowne
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MenuGlowne));
+            this.Wyswietl = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Usun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Wyswietl
+            // 
+            this.Wyswietl.Location = new System.Drawing.Point(678, 150);
+            this.Wyswietl.Name = "Wyswietl";
+            this.Wyswietl.Size = new System.Drawing.Size(135, 41);
+            this.Wyswietl.TabIndex = 0;
+            this.Wyswietl.Text = "Wyświetl samochody";
+            this.Wyswietl.UseVisualStyleBackColor = true;
+            this.Wyswietl.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.25F);
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(323, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(508, 44);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Wypożyczalnia samochodów";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.label2.ForeColor = System.Drawing.Color.DarkRed;
+            this.label2.Location = new System.Drawing.Point(610, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(221, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Co chcesz zrobić?";
+            // 
+            // Usun
+            // 
+            this.Usun.Location = new System.Drawing.Point(678, 217);
+            this.Usun.Name = "Usun";
+            this.Usun.Size = new System.Drawing.Size(135, 40);
+            this.Usun.TabIndex = 3;
+            this.Usun.Text = "Usuń samochód z bazy";
+            this.Usun.UseVisualStyleBackColor = true;
+            this.Usun.Click += new System.EventHandler(this.Usun_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(843, 517);
+            this.Controls.Add(this.Usun);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Wyswietl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Wyswietl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Usun;
+    }
+}
