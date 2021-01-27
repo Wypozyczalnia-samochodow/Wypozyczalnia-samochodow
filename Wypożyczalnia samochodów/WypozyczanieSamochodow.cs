80506848c2a7b3acb8dc5aee552364ebec72fd47
ojoj00 <71120940+ojoj00@users.noreply.github.com>
2021-01-27 13:15:56 +0100

    Wyszukiwanie i poprawianie błędów cz1

--- Wypożyczalnia samochodów/WypozyczanieSamochodow.cs
+++ Wypożyczalnia samochodów/WypozyczanieSamochodow.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+//
 namespace Wypozyczalnia_Samochodow
 {
     public partial class WypozyczanieSamochodow : Form
